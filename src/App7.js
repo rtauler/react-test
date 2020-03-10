be4f00156c208edf4109f490fabc8ed11fd15f3a
rtauler <rtauler@gmail.com>
2020-03-10 20:42:11 +0100

    different states in different buttons

--- src/App7.js
+++ src/App7.js
@@ -7,6 +7,7 @@ class App7 extends React.Component {
             count: 0
         }
         this.handleClick = this.handleClick.bind(this)
+        this.handleClick2 = this.handleClick2.bind(this)
     }
 
     handleClick() {
@@ -17,11 +18,20 @@ class App7 extends React.Component {
         })
     }
 
+    handleClick2() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count * 2
+            }
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.state.count}</h1>
                 <button onClick={this.handleClick}>Change!</button>
+                <button onClick={this.handleClick2}>Change! x2</button>
             </div>
         )
     }
